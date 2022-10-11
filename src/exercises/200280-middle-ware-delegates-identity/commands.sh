ee016cd48e42ccf9df94a7ba3a27dccf03990291
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-10-11 11:28:37 +0530

    New Project

---
+++ src/exercises/200280-middle-ware-delegates-identity/commands.sh
@@ -0,0 +1,12 @@
+
+cd ../../..
+
+cd src/exercises/200280-middle-ware-delegates-identity
+
+pwd
+
+dotnet build ./../../dotnet-apps/00190-WebAppMiddleWareUserId/WebAppMiddleWareUserId.csproj
+
+dotnet run --project ./../../dotnet-apps/00190-WebAppMiddleWareUserId/WebAppMiddleWareUserId.csproj
+
+
