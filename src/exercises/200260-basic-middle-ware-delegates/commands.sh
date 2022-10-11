859f7331e7c2f6ddd408a1956efe473c1669c2e7
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-10-11 10:13:02 +0530

    New Project

---
+++ src/exercises/200260-basic-middle-ware-delegates/commands.sh
@@ -0,0 +1,12 @@
+
+cd ../../..
+
+cd src/exercises/200260-basic-middle-ware-delegates
+
+pwd
+
+dotnet build ./../../dotnet-apps/00170-EmptyWebAppMiddleWareDelegates/EmptyWebAppMiddleWareDelegates.csproj
+
+dotnet run --project ./../../dotnet-apps/00170-EmptyWebAppMiddleWareDelegates/EmptyWebAppMiddleWareDelegates.csproj
+
+
