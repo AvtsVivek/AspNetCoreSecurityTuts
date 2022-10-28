5d09530b1ca722604eb52d39bbc444fbec24242e
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-10-28 18:50:47 +0530

    Added more info

--- src/exercises/220350-WebAppCallSecureApi/commands.sh
+++ src/exercises/220350-WebAppCallSecureApi/commands.sh
@@ -17,4 +17,34 @@ cd src/exercises/220350-WebAppCallSecureApi
 
 dotnet run --project ./../../dotnet-apps/20350-WebAppCallSecureApi/WebApp/WebApp.csproj
 
+################################################################################################
+
+cd ../../..
+
+cd src/dotnet-apps/20350-WebAppCallSecureApi/
+
+Set-Location WebApp
+
+Set-Location ClientApp
+
+npm install
+
+npm run wpbuild
+
+Set-Location ..
+
+pwd
+
+dotnet build ./../WebAppCallSecureApi.sln
+
+dotnet run --project ./../SecureApi/SecureApi.csproj
+
+# Open a new terminal.
+
+cd src/exercises/220350-WebAppCallSecureApi
+
+dotnet run --project ./../../dotnet-apps/20350-WebAppCallSecureApi/WebApp/WebApp.csproj
+
+################################################################################################
+
 
