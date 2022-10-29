9a988d0dd77efa2f9ca13491acc2c68b4eeb7d12
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-10-29 19:56:06 +0530

    New Project

---
+++ src/exercises/220600-WebAppCallApiReuseToken/commands.sh
@@ -0,0 +1,49 @@
+
+cd ../../..
+
+cd src/exercises/220600-WebAppCallApiReuseToken
+
+pwd
+
+################################################################################################
+
+dotnet build ./../../dotnet-apps/20600-WebAppCallApiReuseToken/WebAppCallApiReuseToken.sln
+
+dotnet run --project ./../../dotnet-apps/20600-WebAppCallApiReuseToken/WebAppCallApiReuseToken.sln
+
+# Open a new terminal.
+
+cd src/exercises/220600-WebAppCallApiReuseToken
+
+dotnet run --project ./../../dotnet-apps/20600-WebAppCallApiReuseToken/WebApp/WebApp.csproj
+
+################################################################################################
+
+cd ../../..
+
+cd src/dotnet-apps/20600-WebAppCallApiReuseToken/
+
+Set-Location WebApp
+
+Set-Location ClientApp
+
+npm install
+
+npm run wpbuild
+
+Set-Location ..
+
+pwd
+
+dotnet build ./../WebAppCallApiReuseToken.sln
+
+dotnet run --project ./../SecureApi/SecureApi.csproj
+
+# Open a new terminal.
+
+cd src/exercises/220600-WebAppCallApiReuseToken
+
+dotnet run --project ./../../dotnet-apps/20600-WebAppCallApiReuseToken/WebApp/WebApp.csproj
+
+################################################################################################
+
