fbe51cfff35a91863cbfed1569e6294ddf8adbc7
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-10-30 10:17:24 +0530

    New Project

---
+++ src/exercises/220620-WebAppCallApiWithJwtPolicy/commands.sh
@@ -0,0 +1,49 @@
+
+cd ../../..
+
+cd src/exercises/220620-WebAppCallApiWithJwtPolicy
+
+pwd
+
+################################################################################################
+
+dotnet build ./../../dotnet-apps/20620-WebAppCallApiWithJwtPolicy/WebAppCallApiWithJwtPolicy.sln
+
+dotnet run --project ./../../dotnet-apps/20620-WebAppCallApiWithJwtPolicy/SecureApi/SecureApi.csproj
+
+# Open a new terminal.
+
+cd src/exercises/220620-WebAppCallApiWithJwtPolicy
+
+dotnet run --project ./../../dotnet-apps/20620-WebAppCallApiWithJwtPolicy/WebApp/WebApp.csproj
+
+################################################################################################
+
+cd ../../..
+
+cd src/dotnet-apps/20620-WebAppCallApiWithJwtPolicy/
+
+Set-Location WebApp
+
+Set-Location ClientApp
+
+npm install
+
+npm run wpbuild
+
+Set-Location ..
+
+pwd
+
+dotnet build ./../WebAppCallApiWithJwtPolicy.sln
+
+dotnet run --project ./../SecureApi/SecureApi.csproj
+
+# Open a new terminal.
+
+cd src/exercises/220620-WebAppCallApiWithJwtPolicy
+
+dotnet run --project ./../../dotnet-apps/20620-WebAppCallApiWithJwtPolicy/WebApp/WebApp.csproj
+
+################################################################################################
+
