a97760ee4387710b5f075b5c8b5844725eb39071
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-10-31 17:02:47 +0530

    New Project

---
+++ src/exercises/220720-WebAppIdLogin/ReadMe.md
@@ -0,0 +1,6 @@
+# Web App Aspdotnet core identity.
+
+- This builds on the previous example. That example has a register page to register user.
+- This example adds a login page, so users can login to the app.
+- The login page basically uses a signin manager object to login the user.
+- 
---
+++ src/exercises/220720-WebAppIdLogin/commands.sh
@@ -0,0 +1,56 @@
+
+cd ../../..
+
+cd src/exercises/220720-WebAppIdLogin
+
+pwd
+
+################################################################################################
+
+dotnet build ./../../dotnet-apps/20720-WebAppIdLogin/WebAppIdLogin.sln
+
+dotnet run --project ./../../dotnet-apps/20720-WebAppIdLogin/WebAppIdLogin.csproj
+
+################################################################################################
+
+cd ../../..
+
+cd src/dotnet-apps/20720-WebAppIdLogin/
+
+Set-Location ClientApp
+
+npm install
+
+npm run wpbuild
+
+Set-Location ..
+
+pwd
+
+dotnet build ./WebAppIdLogin.sln
+
+dotnet run --project ./WebAppIdLogin.csproj
+
+# Open a new terminal.
+
+cd src/exercises/220720-WebAppIdLogin
+
+dotnet run --project ./../../dotnet-apps/20720-WebAppIdLogin/WebAppIdLogin.csproj
+
+################################################################################################
+
+# https://learn.microsoft.com/en-us/ef/core/cli/dotnet#update-the-tools
+
+# Ensure a database is created before executing the following migration commands.
+# You can use the following command. CREATE DATABASE AspNetCoreId. If you want to drop and recreate, then 
+# DROP DATABASE AspNetCoreId
+
+cd src/dotnet-apps/20720-WebAppIdLogin/
+
+dotnet tool update --global dotnet-ef
+
+dotnet ef migrations add InitialMigration
+
+dotnet ef database update InitialMigration
+
+dotnet run --project ./WebAppIdLogin.csproj
