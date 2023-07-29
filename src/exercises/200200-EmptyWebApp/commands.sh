05b516da2797a8d13727be14dcc86006bbe1ad28
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-07-29 12:23:51 +0530

    Renamed folders

---
+++ src/exercises/200200-EmptyWebApp/commands.sh
@@ -0,0 +1,11 @@
+
+cd ../../..
+
+cd src/exercises/200200-EmptyWebApp
+
+pwd
+
+dotnet build ./../../dotnet-apps/200200-EmptyWebApp/EmptyWebApp.csproj
+
+dotnet run --project ./../../dotnet-apps/200200-EmptyWebApp/EmptyWebApp.csproj
+
